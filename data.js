b1596e0106404a6d9494450126cb9c6756087caa
Mariia <mariia.bieskova@gmail.com>
2024-07-25 19:08:41 +0300

    generate a separate data.js file containing the menuItems array

---
+++ data.js
@@ -0,0 +1,29 @@
+export const menuItems = [
+    {
+        title: 'Pizza',
+        image: 'assets/pizza.png',
+        description: 'pepperoni, mushroom, mozarella',
+        price: 14,
+        isAdded: false,
+        uuid: '',
+    },
+
+    {
+        title: 'Burger',
+        image: 'assets/burger.png',
+        description: 'beef, cheese, lettuce',
+        price: 12,
+        isAdded: false,
+        uuid: '',
+    },
+    {
+        title: 'Beer',
+        image: 'assets/beer.png',
+        description: 'grain, hops, yeast, water',
+        price: 12,
+        isAdded: false,
+        uuid: '',
+    }
+    
+
+]
