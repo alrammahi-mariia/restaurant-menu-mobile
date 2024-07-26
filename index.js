ac45eaead6f49114d0278c8809fc3b8e27c00488
Mariia <mariia.bieskova@gmail.com>
2024-07-26 13:57:59 +0300

    fixed css in menu items rendering

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ const menuItemsContainer = document.querySelector('.menu-items-container')
 
 function renderMenuItems(items){
     return menuItemsContainer.innerHTML = items.map(item => {
-       return `<div class="item-content">
-        <img src="${item.image}" alt = "${item.title}" class="food-item-image">
-        <div class="item-details">
-            <h2 class="food-item-title" id="food-item-title">${item.title}</h2>
-            <p class="food-item-description" id="food-item-description">${item.description}</p>
-            <p class="food-item-price" id="food-item-price">$${item.price}</p>
-        </div>  
-</div> 
-<div class="add-btn">          
-        <button class="add-btn" id="add-btn">+</button>
-</div>`
+       return `
+       <div class="food-item"
+            <div class="item-content">
+                <div class="item-container">
+                        <img src="${item.image}" alt = "${item.title}" class="food-item-image">
+                        <div class="item-details">
+                            <h2 class="food-item-title" id="food-item-title">${item.title}</h2>
+                            <p class="food-item-description" id="food-item-description">${item.description}</p>
+                            <p class="food-item-price" id="food-item-price">$${item.price}</p>
+                        </div>
+                </div>
+                <div class="add-btn">          
+                    <button class="add-btn" id="add-btn">+</button>
+                </div>
+            </div>
+        </div>`
     }).join("");
 
 }
